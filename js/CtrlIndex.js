a795d3eb3f33c514e2e78097dfadf5d19dc0bc9a
aplicacionUMAM <95113492+aplicacionUMAM@users.noreply.github.com>
2021-12-01 17:50:23 -0600

    Update CtrlIndex.js

--- js/CtrlIndex.js
+++ js/CtrlIndex.js
@@ -1,8 +1,10 @@
-// @ts-nocheck
 import {
   getAuth,
   getFirestore
 } from "../lib/fabrica.js";
+import {
+  urlStorage
+} from "../lib/storage.js";
 import {
   cod,
   muestraError
@@ -10,54 +12,65 @@ import {
 import {
   tieneRol
 } from "./seguridad.js";
-import {
-  urlStorage
-} from "../lib/storage.js";
-const firestore = getFirestore();
+
 /** @type {HTMLUListElement} */
+// @ts-ignore
 const lista = document.
   querySelector("#lista");
+const firestore = getFirestore();
+const daoRol = firestore.
+  collection("Rol");
 const daoAlumno = firestore.
   collection("Alumno");
+const daoUsuario = firestore.
+  collection("Usuario");
 
-getAuth().
-  onAuthStateChanged(
-    protege, muestraError);
+getAuth().onAuthStateChanged(
+  protege, muestraError);
 
 /** @param {import(
     "../lib/tiposFire.js").User}
     usuario */
 async function protege(usuario) {
   if (tieneRol(usuario,
-    ["Artista"])) {
+    ["Administrador"])) {
     consulta();
   }
 }
 
 function consulta() {
-  daoAlumno.
-    orderBy("titulo")
-    .onSnapshot(
-      htmlLista, errConsulta);
+  daoAlumno.onSnapshot(
+    htmlLista, errConsulta);
 }
 
 /**
  * @param {import(
     "../lib/tiposFire.js").
     QuerySnapshot} snap */
-function htmlLista(snap) {
+async function htmlLista(snap) {
   let html = "";
   if (snap.size > 0) {
-    snap.forEach(doc =>
-      html += htmlFila(doc));
+    /** @type {
+          Promise<string>[]} */
+    let usuarios = [];
+    snap.forEach(doc => usuarios.
+      push(htmlFila(doc)));
+    const htmlFilas =
+      await Promise.all(usuarios);
+    /* Junta el todos los
+     * elementos del arreglo en
+     * una cadena. */
+    html += htmlFilas.join("");
   } else {
     html += /* html */
       `<li class="vacio">
-        -- No hay obras registradas. --
+        -- No hay obras
+        registradas. --
       </li>`;
   }
   lista.innerHTML = html;
 }
+
 /**
  * @param {import(
     "../lib/tiposFire.js").
@@ -65,18 +78,14 @@ function htmlLista(snap) {
 async function htmlFila(doc) {
   /**
    * @type {import("./tipos.js").
-                  Alumno} */
+                      Usuario} */
   const data = doc.data();
-  const titulo = cod(data.titulo);
-  const autor = cod(data.autor);
-  const parámetros =
-    new URLSearchParams();
   const img = cod(
     await urlStorage(doc.id));
-  parámetros.append("id", doc.id);
-  return ( /* html */
-    `
-    <div class="col">
+  const titulo = cod(data.titulo);
+  const autor = cod(data.autor);
+  return (/* html */
+    `<div class="col">
           <div class="card h-100">
             <img src="${img}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -90,9 +99,9 @@ async function htmlFila(doc) {
         </div>`);
 }
 
+
 /** @param {Error} e */
 function errConsulta(e) {
   muestraError(e);
   consulta();
 }
-
