46bc21aec947c2022c71693a2f51b51705b319b7
aplicacionUMAM <95113492+aplicacionUMAM@users.noreply.github.com>
2021-12-01 04:46:32 -0600

    Update CtrlIndex.js

--- js/CtrlIndex.js
+++ js/CtrlIndex.js
@@ -1,51 +1,42 @@
+// @ts-nocheck
 import {
   getFirestore
 } from "../lib/fabrica.js";
 import {
-  urlStorage
-} from "../lib/storage.js";
-import {
   cod,
   muestraError
 } from "../lib/util.js";
-
+import {
+  urlStorage
+} from "../lib/storage.js";
+const firestore = getFirestore();
 /** @type {HTMLUListElement} */
-// @ts-ignore
 const lista = document.
   querySelector("#lista");
-const firestore = getFirestore();
 const daoAlumno = firestore.
   collection("Alumno");
 
 
 async function consulta() {
-  daoAlumno.onSnapshot(
-    htmlLista, errConsulta);
+  daoAlumno.
+    orderBy("titulo")
+    .onSnapshot(
+      htmlLista, errConsulta);
 }
 
 /**
  * @param {import(
     "../lib/tiposFire.js").
     QuerySnapshot} snap */
-async function htmlLista(snap) {
+function htmlLista(snap) {
   let html = "";
   if (snap.size > 0) {
-    /** @type {
-          Promise<string>[]} */
-    let usuarios = [];
-    snap.forEach(doc => usuarios.
-      push(htmlFila(doc)));
-    const htmlFilas =
-      await Promise.all(usuarios);
-    /* Junta el todos los
-     * elementos del arreglo en
-     * una cadena. */
-    html += htmlFilas.join("");
+    snap.forEach(doc =>
+      html += htmlFila(doc));
   } else {
     html += /* html */
       `<li class="vacio">
-        -- No hay Obras
-        registradas. --
+        -- No hay obras registradas. --
       </li>`;
   }
   lista.innerHTML = html;
@@ -55,31 +46,37 @@ async function htmlLista(snap) {
  * @param {import(
     "../lib/tiposFire.js").
     DocumentSnapshot} doc */
-async function htmlFila(doc) {
+function htmlFila(doc) {
   /**
    * @type {import("./tipos.js").
-                      Usuario} */
+                  Alumno} */
   const data = doc.data();
+  const titulo = cod(data.titulo);
+  const autor = cod(data.autor);
   const img = cod(
     await urlStorage(doc.id));
   const parámetros =
     new URLSearchParams();
   parámetros.append("id", doc.id);
-  return (/* html */
-    `<li>
-      <a class="fila conImagen"
-          href=${img}">
-        <span class="marco">
-          <img src="${img}"
-            alt="Falta Obra">
-        </span>
-      </a>
-    </li>`);
+  return ( /* html */
+    `
+    <div class="col">
+          <div class="card h-100">
+            <img src="${img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${titulo}</h5>
+              <p class="card-text"><b>Autor:</b>${autor}</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Fecha: </small>
+            </div>
+          </div>
+        </div>`);
 }
-
 
 /** @param {Error} e */
 function errConsulta(e) {
   muestraError(e);
   consulta();
 }
+
